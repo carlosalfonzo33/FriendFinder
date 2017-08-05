a230eb9bec75ac31c60b33f1ada5dbb14ac724ef
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-08-04 19:36:31 -0700

    adjustments

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -7,48 +7,48 @@ var path = require('path');
 // Create Routes
 module.exports = function(app) {
 
-	app.get('/api/friends', function(req, res){
-		res.json(friendsList);
-	});
+    app.get('/api/friends', function(req, res) {
+        res.json(friendsList);
+    });
 
-	app.post('/api/friends', function(req, res){
+    app.post('/api/friends', function(req, res) {
 
-		var bestMatch = {
-			'name': 'none',
-			'photo': 'none'
-		};
+        var bestMatch = {
+            'name': 'none',
+            'photo': 'none'
+        };
 
-		var userTotal = sum(req.body.scores);
+        var userTotal = sum(req.body.scores);
 
-		//console.log(userTotal);
+        //console.log(userTotal);
 
-		var friendTotal = 0;
+        var friendTotal = 0;
 
-			var closest = 50;
+        var closest = 50;
 
-			for (var i = 0; i < friendsList.length; i++) {
-				friendTotal = sum(friendsList[i].scores);
-				var difference = Math.abs(friendTotal - userTotal);
-				if ( difference <= closest ){
-					closest = difference;
-					bestMatch.name = friendsList[i].name;
-					bestMatch.photo = friendsList[i].photo;
-				};
-			};
+        for (var i = 0; i < friendsList.length; i++) {
+            friendTotal = sum(friendsList[i].scores);
+            var difference = Math.abs(friendTotal - userTotal);
+            if (difference <= closest) {
+                closest = difference;
+                bestMatch.name = friendsList[i].name;
+                bestMatch.photo = friendsList[i].photo;
+            };
+        };
 
-		function sum (array) {
-			var total = 0;
-			for (var n = 0; n < array.length; n++) {
-				total += parseInt(array[n]);
-			}
-			return total;
-		}
+        function sum(array) {
+            var total = 0;
+            for (var n = 0; n < array.length; n++) {
+                total += parseInt(array[n]);
+            }
+            return total;
+        }
 
 
-		console.log(bestMatch);
+        console.log(bestMatch);
 
-		res.json(bestMatch);
+        res.json(bestMatch);
 
-	});
+    });
 
 };
